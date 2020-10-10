72ccc3c48a9ed786b4611073831d48238e0079d5
Leo DK <leo.lf.dk@gmail.com>
2020-10-10 15:32:38 +0200

    Added display in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main ()
 {
     Date date = Date (10,10,2020);
+    std::cout << (int)date.getDay() << "/" << (int)date.getMonth() << "/" << date.getYear() << std::endl;
     std::cout << (int)date.computeDay() << std::endl;
     return 0;
 }
